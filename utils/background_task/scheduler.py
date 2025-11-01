f48d20c6710e0d4db90cfcda389d9b0a95190514
Kyra Paraiso <24mashpotato@gmail.com>
2025-11-01 09:44:17 +0800

    adjust mincinno timers

--- utils/background_task/scheduler.py
+++ utils/background_task/scheduler.py
@@ -1,28 +1,34 @@
 from utils.essentials.sched_helper import SchedulerManager
 from utils.background_task.weekly_goal_reset import reset_weekly_goals
 from utils.background_task.daily_ball_reset import daily_ball_reset
+import zoneinfo
+
+NYC = zoneinfo.ZoneInfo("America/New_York")  # auto-handles EST/EDT
+
 # 🛠️ Create a SchedulerManager instance with Asia/Manila timezone
 scheduler_manager = SchedulerManager(timezone_str="Asia/Manila")
 
 async def setup_scheduler(bot):
 
-    # Weekly Goals Reset - Every Sunday at Noon Manila Time
+    # Weekly Goals Reset - Every Sunday at Midnight NYC Time
     scheduler_manager.add_cron_job(
     reset_weekly_goals,
     "weekly_goals_reset",
-    hour=12,
+    hour=0,
     minute=00,
     day_of_week="sun",
     args=[bot],
+    timezone=NYC,
     )
 
-    # Daily Faction Ball Reset - Every day at 12 pm noon Manila Time
+    # Daily Faction Ball Reset - Every day at midnight NYC Time
     scheduler_manager.add_cron_job(
         daily_ball_reset,
         "daily_faction_ball_reset",
-        hour=12,
+        hour=0,
         minute=00,
         args=[bot],
+        timezone=NYC,
     )
 
 
